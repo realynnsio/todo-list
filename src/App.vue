6c057912dd5794cd6fe88e30b519d81976579e34
sion <n.siomusic@gmail.com>
2025-08-08 17:26:27 +0700

    feat: first styling draft

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,51 @@
 <script setup></script>
 
 <template>
-  <router-link to="/">Home</router-link>
+  <nav>
+    <div :class="$style.logoText">
+      <img :class="$style.logoIcon" alt="" src="/Logo.svg" />
+      <img :class="$style.textIcon" alt="" src="/text.svg" />
+    </div>
+    <router-link :class="$style.navLink" to="/">Home</router-link>
+  </nav>
+
   <router-view />
 </template>
 
-<style scoped></style>
+<style module>
+* {
+  --dark-blue: #002ac9;
+  font-family: Roboto, sans-serif;
+}
+
+nav {
+  width: 100%;
+  position: relative;
+  background-color: var(--dark-blue);
+  height: 64px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 16px 72px;
+  box-sizing: border-box;
+  text-align: left;
+  color: #fff;
+}
+
+.navLink {
+  color: white;
+  text-decoration: none;
+  font-size: 1.2rem;
+  padding: 0.5rem 1rem;
+}
+
+.logoText {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+  position: relative;
+  height: 32px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,43 +24,104 @@ function addNewTask() {
 </script>
 
 <template>
-  <h1>To-Do List</h1>
-  <p>Total tasks: {{ taskStore.taskCount }}</p>
-  <p>Pending tasks: {{ taskStore.pendingTasks.length }}</p>
+  <div class="background">
+    <div class="content">
+      <h1>To-Do List</h1>
 
-  <form class="task-form" @submit.prevent="addNewTask">
-    <input type="text" v-model="newTask.title" placeholder="Task title" />
-    <textarea v-model="newTask.description" placeholder="Task description"></textarea>
-    <button type="submit">Add Task</button>
-  </form>
+      <form class="task-form" @submit.prevent="addNewTask">
+        <input type="text" v-model="newTask.title" placeholder="Task title" />
+        <textarea v-model="newTask.description" placeholder="Task description"></textarea>
+        <button type="submit">Add Task</button>
+      </form>
 
-  <ul>
-    <li v-for="task in filteredTasks" :key="task.id">
-      <input type="checkbox" v-model="task.isDone" />
-      <span>
-        <router-link :to="{ name: 'TaskDescription', params: { id: task.id } }">
-          {{ task.title }}
-        </router-link>
-        <p>{{ task.description }}</p>
-      </span>
-      <button @click="taskStore.deleteTask(task.id)">Delete</button>
-    </li>
-  </ul>
+      <div class="task-stats-container">
+        <div class="task-stats">
+          <p>Total tasks: {{ taskStore.taskCount }}</p>
+          <p>Completed tasks: {{ taskStore.taskCount - taskStore.pendingTasks.length }}</p>
+        </div>
 
-  <button @click="hideCompleted = !hideCompleted">
-    {{ hideCompleted ? 'Show Completed' : 'Hide Completed' }}
-  </button>
+        <button @click="hideCompleted = !hideCompleted">
+          {{ hideCompleted ? 'Show Completed' : 'Hide Completed' }}
+        </button>
+      </div>
+
+      <ul>
+        <li v-for="task in filteredTasks" :key="task.id">
+          <input type="checkbox" v-model="task.isDone" />
+          <span>
+            <router-link :to="{ name: 'TaskDescription', params: { id: task.id } }">
+              {{ task.title }}
+            </router-link>
+            <p>{{ task.description }}</p>
+          </span>
+          <button @click="taskStore.deleteTask(task.id)">Delete</button>
+        </li>
+      </ul>
+    </div>
+  </div>
 </template>
 
 <style scoped>
 .task-form {
-  max-width: 400px;
+  width: 100%;
+  position: relative;
   display: flex;
   flex-direction: column;
-  margin-bottom: 20px;
+  align-items: flex-end;
+  justify-content: flex-start;
+}
+
+h1 {
+  font-size: 2rem;
+  margin-bottom: 1rem;
+  color: #fff;
+}
+
+.task-form input,
+.task-form textarea {
+  margin-bottom: 0.5rem;
+  border: 1px solid #fff;
+  border-radius: 4px;
+  resize: none;
+  align-self: stretch;
+  position: relative;
+}
+
+.task-form input {
+  height: 1.8rem;
+}
+
+.task-form textarea {
+  min-height: 8rem;
+}
+
+.task-form button {
+  padding: 0.5rem;
+  background-color: #00cdc7;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+  width: 5rem;
 }
 
 ul li {
   margin-bottom: 3rem;
+}
+
+.background {
+  width: 100%;
+  position: absolute;
+  background: linear-gradient(180deg, #0f36c9, #4f9eff 48.56%, #ccefff 84.13%, #fff);
+  min-height: 100vh;
+  box-sizing: border-box;
+}
+
+.content {
+  position: relative;
+  width: fit-content;
+  min-width: 75%;
+  margin: 0 auto;
+  box-sizing: border-box;
 }
 </style>
